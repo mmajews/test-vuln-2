e56c5c8c142254926baa89879cb6c0ccb390d92c
Marcin Majewski <marcin.majewski@sonarsource.com>
2021-12-22 11:21:39 +0100

    index

--- new-file.py
+++ new-file.py
@@ -3,6 +3,12 @@ print 'test-test-test'
 print 'new-test'
 print 'test'
 print 'test-2'
+
+@app.route('/')
+def index0():
+    module = request.args.get("module")
+    exec("import urllib%s as urllib" % module) # Noncompliant\
+
 @app.route('/')
 def index():
     module = request.args.get("module")
